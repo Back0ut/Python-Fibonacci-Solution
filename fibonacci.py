458f0765212d382cd44fc8425f112d7555fbc80e
Back0ut <karl.alberto-smchs@smgeducation.org>
2024-05-15 04:45:38 -0700

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -6,12 +6,13 @@ class fibonacci_solution():
         for n in range(0, length+1):
             (self.ans).append(self.ans[-1] + self.ans[-2])
 
-given_length = input('Length: ')
+if __name__ == '__main__':
+    given_length = input('Length: ')
 
-try:
-    int(given_length)
-except:
-    print('Error! Length must be number')
+    try:
+        int(given_length)
+    except:
+        print('Error! Length must be number')
 
-fibonacci = fibonacci_solution(given_length)
-print(fibonacci.ans)
+    fibonacci = fibonacci_solution(given_length)
+    print(fibonacci.ans)
